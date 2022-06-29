5c52df70390ab54efb146375803cd81167c44c68
Andy Nowacki <a.nowacki@leeds.ac.uk>
2022-06-29 13:45:38 +0100

    Bump version to v0.1.6

--- src/Mineos.jl
+++ src/Mineos.jl
@@ -5,8 +5,9 @@ The Julia `Mineos` module provides an interface to computing normal
 mode eigenfrequencies using the Mineos software.
 
 # List of functions
-- `eigengfrequencies`: Compute the normal mode eigenfrequencies for
-  a range of radial and angular orders for a 1D model of the Earth.
+- `eigenmodes`: Compute normal mode properties for a range of radial
+  and angular orders for a 1D model of the Earth
+- `eigengfrequencies`: Like `eigenmodes`, but just return eigenfrequencies.
 """
 module Mineos
 
